512a9b3a8857a17486830da4208ba134aa4424f3
gnuoynawh <bizxhy@gmail.com>
2022-07-28 17:50:36 +0900

    #4. LOGIN 2 - save / load username

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+//
 function onLoginSubmit(event) {
     event.preventDefault()
-    console.log(event);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+
+    paintGreeting(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+//
+function paintGreeting(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
+//
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+if (savedUserName === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreeting(savedUserName);
+}
